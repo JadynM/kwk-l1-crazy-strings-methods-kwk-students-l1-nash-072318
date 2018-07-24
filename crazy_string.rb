cb06528c8f72e954d4b4ae9f065ecc3dce8e2d84
JadynM <jadynmiller04@gmail.com>
2018-07-24 18:06:26 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,6 +1,6 @@
 # write your method here
 
 def crazy_strings
-  puts "Friends".swapcase.reverse.gsub(/[s]/, 's' => 'z')
+  puts "Friends".swapcase.reverse.gsub(/[s]/, s => 'z')
 end 
 crazy_strings
